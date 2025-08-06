ccd899e9379ef290afced8bfb1eed82dfa4a14b4
Oleksandr-tkchk <chikatuk@gmail.com>
2025-08-06 20:23:01 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();  // Завантажує змінні середовища
+
+const express = require('express');
+const basicAuth = require('express-basic-auth');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(basicAuth({
+  users: { [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD },
+  challenge: true,
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
