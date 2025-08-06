d78ed432ead814841d2a8fd99cd767cfc52d0bcc
Oleksandr-tkchk <chikatuk@gmail.com>
2025-08-06 21:33:10 +0300

    Опис змін

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();  // Завантажує змінні середовища
+require('dotenv').config();
 
 const express = require('express');
 const basicAuth = require('express-basic-auth');
@@ -7,17 +7,18 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(basicAuth({
-  users: { [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD },
-  challenge: true,
-}));
-
+// Спочатку даємо доступ до статичних файлів
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+// Авторизація лише на GET /
+app.get('/', basicAuth({
+  users: { [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD },
+  challenge: true,
+}), (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
