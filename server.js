ee357d8a2d480fb9008fbca892e2c512e7526aec
Oleksandr-tkchk <chikatuk@gmail.com>
2025-08-06 20:51:33 +0300

    Опис змін

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(basicAuth({
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
