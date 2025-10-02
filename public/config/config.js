a20a3af9482ebd3e6e6fee876dddaa53e7d6d95f
Route <route.project25@gmail.com>
2025-10-02 12:01:42 +0300

    Опис змін

---
+++ public/config/config.js
@@ -0,0 +1,15 @@
+// /config/config.js
+(function () {
+  window.APP_CONFIG = Object.freeze({
+    firebase: {
+      apiKey: "AIzaSyAREP_iNSUZqNFH5RB5PE084b3wNyxaJ6c",
+      authDomain: "route-project-ba7c6.firebaseapp.com",
+      projectId: "route-project-ba7c6",
+      storageBucket: "route-project-ba7c6.firebasestorage.app",
+      messagingSenderId: "953368824279",
+      appId: "1:953368824279:web:68d28c2fd40292c13ee392",
+      measurementId: "G-B6B2JK7BXE"
+    },
+    mapboxToken: "pk.eyJ1Ijoicm91dGUtcHJvamVjdCIsImEiOiJjbWc2ZGlsYnQwYmlrMmpyMnJmdHhya3VhIn0.dTb3s9o92U18k-w5qBBj-Q"
+  });
+})();
