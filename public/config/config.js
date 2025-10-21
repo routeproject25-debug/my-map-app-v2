394a496faafca343bdc0553998ed91f26add2900
Route <route.project25@gmail.com>
2025-10-21 12:15:56 +0300

    Опис змін

--- public/config/config.js
+++ public/config/config.js
@@ -5,7 +5,7 @@
       apiKey: "AIzaSyAREP_iNSUZqNFH5RB5PE084b3wNyxaJ6c",
       authDomain: "route-project-ba7c6.firebaseapp.com",
       projectId: "route-project-ba7c6",
-      storageBucket: "route-project-ba7c6.firebasestorage.app",
+  storageBucket: "route-project-ba7c6.appspot.com",
       messagingSenderId: "953368824279",
       appId: "1:953368824279:web:68d28c2fd40292c13ee392",
       measurementId: "G-B6B2JK7BXE"
